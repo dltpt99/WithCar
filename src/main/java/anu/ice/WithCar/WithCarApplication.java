c6bfc048642efa3d3787408b79130a7522e8c946
LS <LS@me.nt>
2023-03-18 22:13:15 +0900

    DB 관련 설정을 막던 항목 제거

--- src/main/java/anu/ice/WithCar/WithCarApplication.java
+++ src/main/java/anu/ice/WithCar/WithCarApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class WithCarApplication {
 
 	public static void main(String[] args) {
