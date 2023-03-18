a1fa5c7aa1a27f081928e36d467a28f897e4004c
LS <LS@me.nt>
2023-03-18 22:01:01 +0900

    @Repository 어노테이션 추가

--- src/main/java/anu/ice/WithCar/repository/MemberRepository.java
+++ src/main/java/anu/ice/WithCar/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package anu.ice.WithCar.repository;
 
 import anu.ice.WithCar.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByUserId(String user_id);
 
