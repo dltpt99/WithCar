0b5c97ddc6dcd6ee74a5251a3823f2c733927a69
LS <LS@me.nt>
2023-04-20 15:54:26 +0900

    비밀번호 변경 기능 추가

--- src/main/java/anu/ice/WithCar/controller/MemberController.java
+++ src/main/java/anu/ice/WithCar/controller/MemberController.java
@@ -7,6 +7,8 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,6 +26,12 @@ public class MemberController {
     public String showMyInfo(@AuthenticationPrincipal UserDetailsEntity member) {
 
         return new JSONObject(member.getMember()).toString();
+    }
+
+    @PostMapping("/myInfo/change-pw")
+    public Member changePasswordController(@AuthenticationPrincipal UserDetailsEntity member,
+                                           @RequestParam("pw") String pw){
+        return memberService.changePassword(member.getMember(), pw);
     }
 
     @GetMapping("/admin/memberList")
--- src/main/java/anu/ice/WithCar/service/MemberService.java
+++ src/main/java/anu/ice/WithCar/service/MemberService.java
@@ -1,8 +1,10 @@
 package anu.ice.WithCar.service;
 
 import anu.ice.WithCar.domain.entity.Member;
+import anu.ice.WithCar.exception.MemberNotFoundException;
 import anu.ice.WithCar.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,20 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+    public Member changePassword(Member member_, String pw) {
+        Member member = getMember(member_.getIdNumber());
+        member.setPw(new BCryptPasswordEncoder().encode(pw));
+        return saveMemberInfo(member);
+    }
+
+    private Member saveMemberInfo(Member member){
+        return memberRepository.save(member);
+    }
+
+    private Member getMember(long id) {
+        return memberRepository.findById(id).orElseThrow(MemberNotFoundException::new);
+
+    }
     public List<Member> getMemberList() {
         return memberRepository.findAll();
     }
